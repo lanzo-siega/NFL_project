7fd9b233de0cb95165842e0bbc0cbd4d3c964572
lanzo-siega <54330163+lanzo-siega@users.noreply.github.com>
2019-11-25 16:39:43 -0500

    Add files via upload

---
+++ overthecap.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class OverthecapSpider(scrapy.Spider):
+    name = 'overthecap'
+    allowed_domains = ['overthecap.com']
+    start_urls = ['http://overthecap.com/contracts']
+
+    def parse(self, response):
+        for table in response.xpath('/html/body/div[1]/div/div/div[3]/table/tbody'):
+            Player = table.css('tr:nth-child(n) td:nth-child(1) a::text').getall()
+            Position = table.css('tr:nth-child(n) td:nth-child(2)::text').getall()
+            Team = table.css('tr:nth-child(n) td:nth-child(3) a::text').getall()
+            TotalValue = table.css('tr:nth-child(n) td:nth-child(4)::text').getall()
+            AvgPerYear = table.css('tr:nth-child(n) td:nth-child(5)::text').getall()
+            TotGuarantee = table.css('tr:nth-child(n) td:nth-child(6)::text').getall()
+            AvgGuarPerYear = table.css('tr:nth-child(n) td:nth-child(7)::text').getall()
+            PercentGuaranteed = table.css('tr:nth-child(n) td:nth-child(8)::text').getall()
+            
+        for item in zip(Player,Position,Team,TotalValue,AvgPerYear,TotGuarantee,AvgGuarPerYear,PercentGuaranteed):
+            scraped_info = {
+                'Player': item[0],
+                'Position' : item[1],
+                'Team' : item[2],
+                'TotalValue' : item[3],
+                'AvgPerYear' : item[4],
+                'TotGuarantee' : item[5],
+                'AvgGuarPerYear' : item[6],
+                'PercentGuaranteed' : item[7]
+                }
+    
+                #yield or give the scraped info to scrapy
+            yield scraped_info
+            
+           
